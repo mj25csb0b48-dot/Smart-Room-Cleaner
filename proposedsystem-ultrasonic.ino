427befcd77e20564e2fc4b9efb6e0da734b0431d
mj25csb0b48-dot <mj25csb0b48@student.nitw.ac.in>
2026-03-25 01:11:45 +0530

    Update and rename proposedsystem-navigation..ino to proposedsystem-ultrasonic.ino

---
+++ proposedsystem-ultrasonic.ino
@@ -0,0 +1,19 @@
+#define trigPin 9
+#define echoPin 10
+
+long duration;
+int distance;
+
+int getDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+
+  return distance;
+}
